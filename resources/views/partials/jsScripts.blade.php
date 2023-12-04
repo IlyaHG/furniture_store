47412088c6c93e303b76b61ddb49e946f740e740
Ilya <kurat.ilya@gmail.com>
2023-12-04 11:58:57 +0300

    верстка(Илья)

---
+++ resources/views/partials/jsScripts.blade.php
@@ -0,0 +1,9 @@
+
+  <!-- All Script JS Plugins here  -->
+  <script src="assets/js/vendor/popper.js" defer="defer"></script>
+  <script src="assets/js/vendor/bootstrap.min.js" defer="defer"></script>
+  <script src="assets/js/plugins/swiper-bundle.min.js" defer="defer"></script>
+  <script src="assets/js/plugins/glightbox.min.js" defer="defer"></script>
+
+  <!-- Customscript js -->
+  <script src="assets/js/script.js" defer="defer"></script>
